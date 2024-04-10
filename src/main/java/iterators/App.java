ae7e2a12a7c171454e737621f176437604c8237a
s4sikdar <saileshsikdar1@gmail.com>
2024-04-10 08:44:05 -0400

    Added to the loblaws iterator, and made pom.xml changes
    I added to the loblaws iterator, as the original code was just placeholder
    code.
    1) I added to the LoblawsIterator.java file the code for opening the
    webdriver at a given url, going to the store locator and typing "Canada"
    to get all locations in Canada, and then getting each city. For each city,
    I stored the city as a key in a hashmap, with its value being
    Boolean(true). This serves to get all city locations for all stores, so
    that a store can be selected for each city and the prices be scraped for
    all cities. The reason for this is because prices at stores in
    different cities can be different. This ensures that all price
    discrepancies are accounted for across different cities. The plan will
    be to type in each city in the store locator, and select the first store
    that shows in that city. For that city, prices will be scraped from that
    store only, as prices are not expected to change across different stores
    in the same city.
    2) I added changes to the pom.xml file to include selenium and other
       libraries related to selenium.
    3) Since the LoblawsIterator.java code will be scraping the website at a
       given url, there are a lot values that would best be taken from a
       .properties file (such as the url address, css selector values of
       various html elements, etc.). Otherwise, they would be hard-coded
       into the code itself, which requires more maintenance (i.e. the code
       has to repeatedly be compiled). The hard-coded values are stored in
       config/loblaws.properties.
    4) I added the target/ directory in the .gitignore file, as that is the
       output of running maven commands to compile and package the project.
    5) I used the dos2unix command on all of the files in this commit due to
       line endings being different. So some files have no change but are in
       this commit for that reason. Other files have the entire old lines
       plus the new lines being in the commit for the same reason.

--- src/main/java/iterators/App.java
+++ src/main/java/iterators/App.java
@@ -9,13 +9,16 @@ import iterators.loblaws.LoblawsIterator;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         //System.out.println( "Hello World!" );
 	String currentPath = System.getProperty("user.dir");
 	Path pwd = Paths.get(currentPath);
+	Path firefoxdriver_path = pwd.resolve("drivers");
+	firefoxdriver_path = firefoxdriver_path.resolve("geckodriver.exe");
+	System.setProperty("webdriver.firefox.driver", firefoxdriver_path.toString());
 	pwd = pwd.resolve("config");
 	pwd = pwd.resolve("loblaws.properties");
 	//String config_path = pwd.toString() + "loblaws.properties";
--- src/main/java/iterators/loblaws/LoblawsIterator.java
+++ src/main/java/iterators/loblaws/LoblawsIterator.java
@@ -1,10 +1,33 @@
 package iterators.loblaws;
+import java.lang.*;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import iterators.GroceryStorePriceScraper;
 
 public class LoblawsIterator implements GroceryStorePriceScraper {
@@ -13,11 +36,14 @@ public class LoblawsIterator implements GroceryStorePriceScraper {
 	private String fpath;
 	private HashMap<String, String> numbers;
 	private Properties configurations;
+	private WebDriver driver;
+	private HashMap<String, Boolean> cities;
 
 	public LoblawsIterator(String config_file_path, int count, int limit) {
 		this.counter = count;
 		this.limit = limit;
 		this.numbers = new HashMap<>();
+		this.cities = new HashMap<>();
 		this.fpath = config_file_path;
 		File filename = new File(config_file_path);
 		//System.out.println(new File(".").getAbsolutePath());
@@ -32,6 +58,7 @@ public class LoblawsIterator implements GroceryStorePriceScraper {
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
+		this.driver = null;
 		//this.configurations = new Properties();
 		//this.configurations.load(new FileInputStream(filename));
 
@@ -50,8 +77,138 @@ public class LoblawsIterator implements GroceryStorePriceScraper {
 		return (this.counter <= this.limit);
 	}
 
-	public String getUrl() {
+	// Code was taken from below stackoverflow link:
+	// https://stackoverflow.com/questions/12858972/how-can-i-ask-the-selenium-webdriver-to-wait-for-few-seconds-in-java
+	public WebElement fluentWait(final By locator, WebDriver driver, int timeout_duration, long polling_duration) {
+	    Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+		    .withTimeout(Duration.ofSeconds(timeout_duration))
+		    .pollingEvery(Duration.ofMillis(polling_duration))
+		    .ignoring(NoSuchElementException.class);
+
+	    WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+		public WebElement apply(WebDriver driver) {
+		    return driver.findElement(locator);
+		}
+	    });
+
+	    return foo;
+	};
+
+	public boolean elementExists(final By locator, WebDriver driver, int timeout_duration, long polling_duration) {
+		try {
+			WebElement element_to_find = this.fluentWait(
+				locator, driver, timeout_duration, polling_duration
+			);
+			if (element_to_find != null) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (Throwable err) {
+			return false;
+		}
+	}
+
+	public void getAllCities() throws InterruptedException {
+		String location_button_selector = this.configurations.getProperty("location_button");
+		String change_location_selector = this.configurations.getProperty("change_location_button");
+		WebElement location_button = this.fluentWait(
+			new By.ByCssSelector(location_button_selector), this.driver, 30, 1000L
+		);
+		this.driver.manage().window().maximize();
+		location_button.click();
+		WebElement change_location_button = this.fluentWait(
+			new By.ByCssSelector(change_location_selector), this.driver, 30, 1000L
+		);
+		change_location_button.click();
+		WebElement location_input = this.fluentWait(
+			new By.ByCssSelector(this.configurations.getProperty("location_input_field")),
+			this.driver, 30, 1000L
+		);
+		new Actions(this.driver)
+			.moveToElement(location_input)
+			.click()
+			.sendKeys(this.configurations.getProperty("location_input_field_value"))
+			.pause(Duration.ofMillis(1000))
+			.sendKeys(Keys.ENTER)
+			.pause(Duration.ofMillis(500))
+			.perform();
+		int counter = 1;
+		StringBuilder store_info_container_selector = new StringBuilder(
+			this.configurations.getProperty("store_content_box_selector")
+		);
+		StringBuilder store_location_text_selector = new StringBuilder(
+			this.configurations.getProperty("store_location_text_selector")
+		);
+		String privacy_policy_selector = this.configurations.getProperty("privacy_policy_selector");
+		WebElement privacy_policy_close_button = this.fluentWait(
+			new By.ByCssSelector(privacy_policy_selector), this.driver, 30, 1000L
+		);
+		new Actions(this.driver)
+			.moveToElement(privacy_policy_close_button)
+			.click()
+			.pause(Duration.ofMillis(1000))
+			.perform();
+		while (
+			this.elementExists(
+				new By.ByCssSelector(store_info_container_selector.toString()), this.driver, 30, 500L
+			)
+		) {
+			WebElement store_info_container = this.fluentWait(
+				new By.ByCssSelector(store_info_container_selector.toString()), this.driver, 30, 500L
+			);
+			WebElement store_info_location_text = store_info_container.findElement(
+				new By.ByCssSelector(store_location_text_selector.toString())
+			);
+			String store_location_value = store_info_location_text.getText();
+			String[] city_and_province = store_location_value.split(
+				"[A-Za-z]\\d[A-Za-z][ -]?\\d[A-Za-z]\\d$"
+			);
+			city_and_province[0] = city_and_province[0].trim();
+			String city_province_combination = city_and_province[0];
+			this.cities.putIfAbsent(city_province_combination, new Boolean(true));
+			int num_index = store_info_container_selector.indexOf(Integer.toString(counter));
+			int num_length = Integer.toString(counter).length();
+			counter++;
+			store_info_container_selector.replace(
+				num_index, (num_index + num_length), Integer.toString(counter)
+			);
+		}
+
+		Set<String> unique_cities = this.cities.keySet();
+		for (String city: unique_cities) {
+			WebElement location_input_button = this.fluentWait(
+				new By.ByCssSelector(this.configurations.getProperty("location_input_field")),
+				this.driver, 30, 1000L
+			);
+			new Actions(this.driver)
+				.moveToElement(location_input_button)
+				.click()
+				.sendKeys(city)
+				.pause(Duration.ofMillis(500))
+				.sendKeys(Keys.ENTER)
+				.pause(Duration.ofMillis(750))
+				.perform();
+			String branch_info_selector = this.configurations.getProperty("store_content_box_selector");
+			WebElement store_info_container = this.fluentWait(
+				new By.ByCssSelector(branch_info_selector), this.driver, 30, 1000L
+			);
+		}
+
+	}
+
+	public String getUrl() throws InterruptedException {
+		FirefoxOptions options = new FirefoxOptions();
+		// https://stackoverflow.com/questions/13959704/accepting-sharing-location-browser-popups-through-selenium-webdriver
+		options.addPreference("geo.prompt.testing", true);
+		options.addPreference("geo.prompt.testing.allow", true);
+		//options.addPreference(
+		//	"geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%"
+		//);
+		this.driver = new FirefoxDriver(options);
+		this.driver.get(this.configurations.getProperty("url"));
+		this.getAllCities();
+		this.driver.quit();
 		return this.configurations.getProperty("url");
-		//return this.fpath;
 	}
 }
