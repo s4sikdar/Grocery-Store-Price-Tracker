47e3c11a3d2efe923f9833926d96f06ca09ba588
s4sikdar <saileshsikdar1@gmail.com>
2024-06-17 14:22:44 -0400

    Added code for the Metro Iterator
    Added starting code for the Iterator for metro-based stores

---
+++ src/main/java/iterators/metro/MetroIterator.java
@@ -0,0 +1,41 @@
+package iterators.metro;
+import java.lang.*;
+import java.util.*;
+import java.time.*;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.DirectoryStream;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import javax.xml.stream.XMLStreamException;
+import iterators.GroceryStorePriceScraper;
+import iterators.xml.*;
+
+
+public class LoblawsIterator implements GroceryStorePriceScraper {}
