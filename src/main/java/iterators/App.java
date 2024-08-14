9543b7819154d531cee5db550ee6c92f7c983de9
s4sikdar <saileshsikdar1@gmail.com>
2024-08-13 22:57:44 -0400

    Added code to separate XML results into different files for food groups
    I added code to separate XML results into different files for the
    different categories of food groups. The categories of food groups are
    based on the table names in the database. There are different XML
    parsers for the different XML files (one for each file representing a
    food group). Once the scraping is done, the results are iterated over
    and based on the category, they are put into the different XML files.
    
    I also added a query method to the DatabaseClient that allows you to to
    specify a query for the DatabaseClient to run. You pass in the SQL code
    you wish to run, and the DatabaseClient runs the query for you. So far
    it has only insert statements in mind, which is why the method has a
    return type of void. In the future more will be done to the
    DatabaseClient class. For example, accommodations will be made to
    include SELECT statements. Another important point to look into is
    handling the case where the results returned from the SELECT statement
    span more memory than is available in your machine's RAM section.

--- src/main/java/iterators/App.java
+++ src/main/java/iterators/App.java
@@ -1,5 +1,8 @@
 package iterators;
 
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.xml.stream.XMLEventFactory;
@@ -14,6 +17,8 @@ import javax.xml.stream.events.XMLEvent;
 import iterators.GroceryStorePriceScraper;
 import iterators.DatabaseClient;
 import iterators.loblaws.LoblawsIterator;
+import iterators.xml.*;
+import iterators.util.*;
 
 /**
  * Hello world!
@@ -30,12 +35,79 @@ public class App
 	firefoxdriver_path = firefoxdriver_path.resolve("geckodriver.exe");
 	System.setProperty("webdriver.firefox.driver", firefoxdriver_path.toString());
 	pwd = pwd.resolve("config");
-	pwd = pwd.resolve("loblaws.properties");
+
+	Path database_properties_path = pwd.resolve("database.properties");
+	Path database_xml_properties_path = pwd.resolve("database_xml.properties");
+	Path loblaws_properties_path = pwd.resolve("nofrills.properties");
+
+	String database_properties_file_path = database_properties_path.toString();
+	String database_xml_properties_file_path = database_xml_properties_path.toString();
+
+	//Properties database_config = new Properties();
+	Properties database_xml_config = new Properties();
+
+	try {
+		//database_config.load(new FileInputStream(database_properties_file_path));
+		database_xml_config.load(new FileInputStream(database_xml_properties_file_path));
+	} catch (Throwable err) {
+		err.printStackTrace();
+		System.exit(1);
+	}
+
+	String fruits_and_vegetables_fname = database_xml_config.getProperty("fruits_and_vegetables");
+	String dairy_and_eggs_fname = database_xml_config.getProperty("dairy_and_eggs");
+	String pantry_fname = database_xml_config.getProperty("pantry");
+	String meat_fname = database_xml_config.getProperty("meat");
+	String snacks_and_chips_and_candy_fname = database_xml_config.getProperty("snacks_and_chips_and_candy");
+	String frozen_food_fname = database_xml_config.getProperty("frozen_food");
+	String bakery_fname = database_xml_config.getProperty("bakery");
+	String drinks_fname = database_xml_config.getProperty("drinks");
+	String deli_fname = database_xml_config.getProperty("deli");
+	String fish_and_seafood_fname = database_xml_config.getProperty("fish_and_seafood");
+	String root_xml_tag_name = database_xml_config.getProperty("root_xml_tag");
+	String mapping_tag_name = database_xml_config.getProperty("mapping_tag");
+	HashMap<String, XMLParser> xml_parsers = new HashMap<>();
+
+	XMLParser fruits_and_vegetables = new XMLParser(fruits_and_vegetables_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser dairy_and_eggs = new XMLParser(dairy_and_eggs_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser pantry = new XMLParser(pantry_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser meat = new XMLParser(meat_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser snacks_and_chips_and_candy = new XMLParser(snacks_and_chips_and_candy_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser frozen_food = new XMLParser(frozen_food_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser bakery = new XMLParser(bakery_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser drinks = new XMLParser(drinks_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser deli = new XMLParser(deli_fname, root_xml_tag_name, mapping_tag_name);
+	XMLParser fish_and_seafood = new XMLParser(fish_and_seafood_fname, root_xml_tag_name, mapping_tag_name);
+	xml_parsers.put("fruits & vegetables", fruits_and_vegetables);
+	xml_parsers.put("dairy & eggs", dairy_and_eggs);
+	xml_parsers.put("pantry", pantry);
+	xml_parsers.put("meat", meat);
+	xml_parsers.put("snacks, chips & candy", snacks_and_chips_and_candy);
+	xml_parsers.put("frozen food", frozen_food);
+	xml_parsers.put("bakery", bakery);
+	xml_parsers.put("drinks", drinks);
+	xml_parsers.put("deli", deli);
+	xml_parsers.put("fisn & seafood", fish_and_seafood);
+	Set<String> xml_parsers_keys = xml_parsers.keySet();
 	//String config_path = pwd.toString() + "loblaws.properties";
-	LoblawsIterator loblaws_iter = new LoblawsIterator(pwd.toString(), 0, 10);
+	LoblawsIterator loblaws_iter = new LoblawsIterator(loblaws_properties_path.toString(), 0, 30);
 	loblaws_iter.loadXML();
 	while (loblaws_iter.hasNext()) {
-		System.out.println(loblaws_iter.next());
+		HashMap<String, String> product_info = loblaws_iter.next();
+		String category = product_info.get("category_path");
+		category = category.toLowerCase().split(">")[1];
+		for (String key: xml_parsers_keys) {
+			if (category.equalsIgnoreCase(key)) {
+				try {
+					xml_parsers.get(key).hashmapToXML(product_info);
+				} catch (XMLStreamException err) {
+					err.printStackTrace();
+				}
+			}
+		}
+	}
+	for (String key: xml_parsers_keys) {
+		xml_parsers.get(key).closeProductXmlOutputStream();
 	}
     }
 }
--- src/main/java/iterators/DatabaseClient.java
+++ src/main/java/iterators/DatabaseClient.java
@@ -5,19 +5,42 @@ import javax.sql.*;
 import java.sql.*;
 import com.mysql.cj.jdbc.Driver;
 
+
 class DatabaseClient {
+	private String dbms_name;
 	private String host_name;
 	private String port_no;
 	private String database_name;
 	private String user;
 	private String pass;
 
-	public DatabaseClient(String host_name, String port_no, String database_name, String user, String pass) {
+
+	public DatabaseClient(String dbms_name, String host_name, String port_no, String database_name, String user, String pass) {
+		this.dbms_name = dbms_name;
 		this.host_name = host_name;
 		this.port_no = port_no;
 		this.database_name = database_name;
 		this.user = user;
 		this.pass = pass;
 	}
+
+
+	/**
+	 * query - a public method that will connect to the database and execute the sql query passed in
+	 * @param sql_text - the String representing the sql you intend to run
+	 * @return - returns nothing (void)
+	 */
+	public void query(String sql_text) {
+		String database_address = "jdbc:" + this.dbms_name + "://" + this.host_name + ":" +
+					this.port_no + "/" + this.database_name;
+		try (Connection con = DriverManager.getConnection(database_address, this.user, this.pass)) {
+			try (Statement stmt = con.createStatement()) {
+				stmt.execute(sql_text);
+			}
+		} catch (SQLException err) {
+			err.printStackTrace();
+		}
+	}
+
 
 }
